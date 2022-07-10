8f3664ff525f157a764a48faee174243847241e4
jamornP <100953989+jamornP@users.noreply.github.com>
2022-07-10 06:53:22 -0700

    theme & composer

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Web-App</title>
+    <link rel="stylesheet" href="/theme/css/bootstrap-theme.css">
 </head>
 <body>
     
